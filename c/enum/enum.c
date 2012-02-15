a15adbf94fbda7e20842c3a1dea4ef241c1a39f1
Michael DeLibero <mldelibero@gmail.com>
2012-02-15 09:57:57 -0500

    Added to enum.c

--- c/enum/enum.c
+++ c/enum/enum.c
@@ -14,10 +14,18 @@ typedef enum state_enum {
 } state_t;
 
 
+enum val {
+    first,
+    second,
+    third
+};
+
+
 status_t status;
 state_t state;
 
 int main(void) {
+    
     printf("good = %i\n",good);
     printf("bad = %i\n",bad);
     printf("indet = %i\n\n",indet);
@@ -25,7 +33,22 @@ int main(void) {
     printf("idle = %i\n",idle);
     printf("stopped = %i\n",stopped);
     printf("going = %i\n",going);
-    printf("pwdDwn = %i\n",pwrDwn);
+    printf("pwdDwn = %i\n\n",pwrDwn);
 
+    printf("first = %i\n",first);
+    printf("second = %i\n",second);
+    printf("third = %i\n",third);
+
+    int cur = 0; // 0 == stopped
+    if (cur == 0) { // if stopped
+        printf("Stopped\n");
+    }
+
+    int current = stopped;
+    
+    if (current == stopped) {
+        printf("Stopped\n");
+    }
+    
     return(0);
 }
